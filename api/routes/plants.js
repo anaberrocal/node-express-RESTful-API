48993f5df1e6dbc13afc33fdf6c59311ff63b1b1
Ana Berrocal <anaberrocaldev@gmail.com>
2020-11-25 11:53:50 -0500

    Add routes to API

--- api/routes/plants.js
+++ api/routes/plants.js
@@ -1,9 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/plants', (req, res, next) => {
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    code: "noice",
+    message: "Getting the plansts you requested...",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    code: "noice",
+    message: "Creating the plansts you requested...",
+  });
+});
+
+router.get("/:plantId", (req, res, next) => {
+  const id = req.params.plantId;
+  if (id === "unique") {
     res.status(200).json({
-        code: "noice",
-        message: "I hope"
-    })
-})
+      code: "noice",
+      message: "Getting you that unique plant you requested...",
+      id: id,
+    });
+  } else {
+      res.status(200).json({
+          code: "Noice",
+          message: "Here's the plant with the id you requested",
+      })
+  }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const plantRoutes = require('./api/routes/plants');
 
 
 //Sets up a middleware so incoming requests have to go through whatever is passed into use
-app.use((req, res, next) => {
-  res.status(200).json({
-    code: "noice",
-    message: "Amazing. Look at you go!",
-  });
-});
+app.use('/plants', plantRoutes);
 
 module.exports = app;
