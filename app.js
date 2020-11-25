c1879545f9d520e2583964c12d061e5cc563629a
Ana Berrocal <anaberrocaldev@gmail.com>
2020-11-25 13:38:11 -0500

    Add morgan module

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
 
 const plantRoutes = require('./api/routes/plants');
 const ordersRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 //Sets up a middleware so incoming requests have to go through whatever is passed into use
 app.use('/plants', plantRoutes);
